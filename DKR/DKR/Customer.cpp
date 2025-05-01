988ec8c7b5e6bd3e4a3f6d9f2660be3cf27902cd
Ivan Kuznetsov <freeplay20055002@gmail.com>
2025-05-01 12:09:38 +0300

    include getters and setters and constructors

--- DKR/DKR/Customer.cpp
+++ DKR/DKR/Customer.cpp
@@ -1,2 +1,61 @@
 #include "Customer.h"
+#include <iostream>
+#include <algorithm>
 
+using namespace std;
+
+Customer::Customer(int Id, string LastName, string FirstName, int CardNum, int BankAccNum)
+	:m_Id(Id), m_LastName(LastName), m_FirstName(FirstName), m_CardNum(CardNum), m_BankAccNum(BankAccNum){}
+
+Customer::Customer()
+	:m_Id(0000), m_LastName("NoName"), m_FirstName("NoName"), m_CardNum(0000), m_BankAccNum(00000){}
+
+void Customer::ExitOfNames()
+{
+
+}
+
+void Customer::IntervalOfCards()
+{
+
+}
+
+void Customer::setId(int newId) {
+    m_Id = newId;
+}
+
+void Customer::setLastName(const std::string& newLastName) {
+    m_LastName = newLastName;
+}
+
+void Customer::setFirstName(const std::string& newFirstName) {
+    m_FirstName = newFirstName;
+}
+
+void Customer::setCreditCardNumber(int newCardNumber) {
+    m_CardNum = newCardNumber;
+}
+
+void Customer::setBankAccountNumber(int newAccountNumber) {
+    m_BankAccNum = newAccountNumber;
+}
+
+int Customer::getId() const {
+    return m_Id;
+}
+
+std::string Customer::getLastName() const {
+    return m_LastName;
+}
+
+std::string Customer::getFirstName() const {
+    return m_FirstName;
+}
+
+int Customer::getCreditCardNumber() const {
+    return m_CardNum;
+}
+
+int Customer::getBankAccountNumber() const {
+    return m_BankAccNum;
+}
--- DKR/DKR/Customer.h
+++ DKR/DKR/Customer.h
@@ -10,5 +10,21 @@ class Customer
 	string m_FirstName;
 	int m_CardNum;
 	int m_BankAccNum;
+
+public:
+	Customer(int Id, string LastName, string FirstName, int CardNum, int BankAccNum);
+	Customer();
+	void ExitOfNames();
+	void IntervalOfCards();
+	void setId(int newId);
+	void setLastName(const std::string& newLastName);
+	void setFirstName(const std::string& newFirstName);
+	void setCreditCardNumber(int newCardNumber);
+	void setBankAccountNumber(int newAccountNumber);
+	int getId() const;
+	std::string getLastName() const;
+	std::string getFirstName() const;
+	int getCreditCardNumber() const;
+	int getBankAccountNumber() const;
 };
 
